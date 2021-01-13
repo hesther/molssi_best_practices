fc4cc30acd8cae67099b565bf0db7177fc31bfa0
Esther Heid <esther_heid@web.de>
2021-01-13 10:40:58 -0500

    first CI

--- molecool/functions.py
+++ molecool/functions.py
@@ -205,6 +205,58 @@ atom_colors = {
     "S": "yellow",
 }
 
+def calculate_molecular_mass(symbols):
+   """Calculate the mass of a molecule.
+   
+   Parameters
+   ----------
+   symbols : list
+       A list of elements.
+   
+   Returns
+   -------
+   mass : float
+       The mass of the molecule
+   """
+
+   mass = 0
+   for symbol in symbols:
+       mass += atomic_weights[symbol]
+   return  mass
+
+def calculate_center_of_mass(symbols, coordinates):
+    """Calculate the center of mass of a molecule.
+   
+    The center of mass is weighted by each atom's weight.
+    
+    Parameters
+    ----------
+    symbols : list
+       A list of elements for the molecule
+    coordinates : np.ndarray
+       The coordinates of the molecule.
+    
+    Returns
+    -------
+    center_of_mass: np.ndarray
+       The center of mass of the molecule.
+    
+    Notes
+    -----
+    The center of mass is calculated with the formula
+    
+    .. math:: \\vec{R}=\\frac{1}{M} \\sum_{i=1}^{n} m_{i}\\vec{r_{}i}
+    
+    """
+
+    center_of_mass = np.array([0., 0., 0.])
+    for i in range(len(symbols)):
+        center_of_mass += atomic_weights[symbols[i]] * coordinates[i]
+    
+    center_of_mass /= calculate_molecular_mass(symbols)
+    
+    return center_of_mass
+
 
 def canvas(with_attribution=True):
     """
--- molecool/tests/test_molecool.py
+++ molecool/tests/test_molecool.py
@@ -98,3 +98,23 @@ def test_build_bond_list_failure(methane_molecule):
 def test_molecool_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "molecool" in sys.modules
+
+
+def test_molecular_mass():
+    symbols = ['C', 'H', 'H', 'H', 'H']
+
+    calculated_mass = molecool.calculate_molecular_mass(symbols)
+
+    actual_mass = 16.04
+
+    assert pytest.approx(actual_mass, abs=1e-2) == calculated_mass
+
+def test_center_of_mass():
+    symbols = np.array(['C', 'H', 'H', 'H', 'H'])
+    coordinates = np.array([[1,1,1], [2.4,1,1], [-0.4, 1, 1], [1, 1, 2.4], [1, 1, -0.4]])
+
+    center_of_mass = molecool.calculate_center_of_mass(symbols, coordinates)
+
+    expected_center = np.array([1,1,1])
+
+    assert np.array_equal(center_of_mass,expected_center)
